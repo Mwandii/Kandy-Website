f8dd1e5ed7f988ebe9abfdf96c54c21d5a1fa322
Athanas Muinde <athanasmwandi5@gmail.com>
2025-11-28 13:58:30 +0300

    add tailwind to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,15 @@
 function Navbar() {
   return (
-    <>
+    <div className="flex justify-between p-4 bg-white shadow-md">
+    <h1 className="font-bold">Kandy Baby Shop</h1>
     <nav>
-    <h1>Kandy Baby Shop</h1>
-    <ul>
+    <ul className="flex gap-4 mr-3">
       <li>About</li>
       <li>Contact Us</li>
       <li>Login</li>
     </ul>
     </nav>
-    </>
+    </div>
   )
 }
 
