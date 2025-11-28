211a74e412cb7d4bc3c2bf310874f18baf075409
Athanas Muinde <athanasmwandi5@gmail.com>
2025-11-28 13:49:12 +0300

    set up tailwindcss

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <>
+    <nav>
+    <h1>Kandy Baby Shop</h1>
+    <ul>
+      <li>About</li>
+      <li>Contact Us</li>
+      <li>Login</li>
+    </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar;
